636306681758b821b5b79b467c9fc6cb5ebaa9c5
NormaArdhitaVidyiawati <normaardhita2019@gmail.com>
2026-06-12 10:25:02 +0700

    Update CORS

--- src/server.js
+++ src/server.js
@@ -25,12 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://frostmart-frontend-lfjh-51uld8eqt.vercel.app",
-    ],
-    allowedHeaders: ["Authorization", "Content-Type"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    origin: true,
     credentials: true,
   }),
 );
